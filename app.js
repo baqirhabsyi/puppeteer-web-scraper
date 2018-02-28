8cf90638c96ca931624e24d8d07759d7da10c028
baqirhabsyi <darthbadar@live.com>
2018-02-28 19:06:23 +0700

    added other to app js

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const app = express();
 const port = process.env.PORT || 8080;
 const run = require('./hotel/traveloka');
 const pegipegi = require('./hotel/pegipegi');
+const traveMobi = require('./hotel/traveloka-mobile');
+const xxi = require('./movies/xxi');
 
 app.listen(port, () => {
   console.log('App running on port ', port);
 
   run();
   pegipegi();
-  // setInterval(run, 7200000);
-  // setInterval(pegipegi, 720000);
+  traveMobi();
+  xxi();
+  setInterval(run, 7200000);
+  setInterval(traveMobi, 720000);
+  setInterval(xxi, 82800000);
+  setInterval(pegipegi, 720000);
 });
 
 app.get('/', (req, res) => {
